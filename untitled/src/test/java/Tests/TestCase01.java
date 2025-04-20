addd164e1393ca7be8da7cec66501c87deda14b2
NarasayyaShetty <NarasayyaShetty497@gmail.com>
2025-04-21 00:29:33 +0530

    changes in xml file
    Initial changes

--- untitled/src/test/java/Tests/TestCase01.java
+++ untitled/src/test/java/Tests/TestCase01.java
@@ -1,4 +1,20 @@
 package Tests;
 
+import SingleTon.DriverSingleTon;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class TestCase01 {
+    WebDriver driver=null;
+    @BeforeTest
+    @Parameters({"browserName"})
+    public void initSetup(String browserName){
+        driver=DriverSingleTon.driverInit(browserName);
+    }
+    @Test(description = "testcaseo1")
+    public void testcase01(){
+        driver.get("https://crio-qkart-frontend-qa.vercel.app/");
+    }
 }
